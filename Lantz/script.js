698bc19e7ec83fadea290e896ba4211e03530f5f
pablopintos <pabbe_spain@hotmail.com>
2022-04-21 14:07:08 +0200

    mapss

--- Lantz/script.js
+++ Lantz/script.js
@@ -6,3 +6,15 @@ document.querySelector(".close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 })
 
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
