b0393c02a181e2d7e15473f32736f520036fba18
pablopintos <pabbe_spain@hotmail.com>
2022-05-17 17:08:37 +0200

    maps

--- HB_test_maskin/static/app.js
+++ HB_test_maskin/static/app.js
@@ -1,22 +1,40 @@
 
-
+var geocoder;
+var map;
+/*
+  var adress = {{ address_list }};
+*/
+var address = [
+    "Måsvägen 12b, lund",
+    "rapsvägen 115, arlöv",
+    "dalbyvägen 37, arlöv"
+  ];
 function initMap() {
-  const myLatLng = { lat: 55.604980, lng: 13.003822  };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: myLatLng,
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 55.604980, lng: 13.003822 }
   });
-  const image =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  geocoder = new google.maps.Geocoder();
 
-
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello Worsadsdsassdasld!",
-    icon:image,
-  });
-  
+  for (let i = 0; i < address.length; i++) {
+      codeAddress(geocoder, map, address[i]);
+  }
+  codeAddress(geocoder, map);
 }
+
+function codeAddress(geocoder, map, address) {
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
 
 window.initMap = initMap;
